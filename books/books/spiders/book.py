710b2fbca38d96194c9b702e753d1323d4696f4b
clangv <issayvega@gmail.com>
2025-04-27 22:26:27 +0800

    fixed pagination and deduplication.

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -4,13 +4,13 @@ from books.items import BooksItem
 
 class BookSpider(scrapy.Spider):
     name = "book"
-    allowed_domains = ["book.toscrape.com"]
-    start_urls = ["https://books.toscrape.com/"]
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/catalogue/page-1.html"]
 
     def parse(self, response):
         for book in response.css("article.product_pod"):
             item = BooksItem()
-            item["url"] = book.css("h3 a::attr(href)").get()
+            item["url"] = response.urljoin(book.css("h3 a::attr(href)").get())
             item["title"] = book.css("h3 a::attr(title)").get()
             item["price"] = book.css("p.price_color::text").get()
             yield item
@@ -18,6 +18,6 @@ class BookSpider(scrapy.Spider):
         next_page = response.css("li.next > a::attr(href)").get()
         if next_page:
             next_page_url = response.urljoin(next_page)
-            yield scrapy.Request(url=next_page_url, callback=self.parse)
+        yield scrapy.Request(url=next_page_url, callback=self.parse)
 
-        
+            
