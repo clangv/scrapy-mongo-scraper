1d38603d529ccf7df312c6c923218abe9347bc9c
clangv <issayvega@gmail.com>
2025-05-03 01:03:02 +0800

    The .setUp() method initializes a BookSpider instance before each test, creates an example HTML string, containing two book entries and a link to the next page. It then mocks a request to the site by returning an HtmlResponse object with that sample HTML.

--- books/books/test_book.py
+++ books/books/test_book.py
@@ -1,6 +1,19 @@
 import unitest
+from scrapy.http import HtmlResponse
+from books.spider.book import BookSpider
 
 class BookSpiderTest(unitest.TestCase):
+
+    def setUp(self):
+        self.spider = BookSpider()
+        self.example_html = """
+            Insert the example HTML here
+    """
+        self.response =HtmlResponse(
+            url="https://books.toscrape.com",
+            body=self.example_html,
+            encoding="utf-8"
+        )
     def test_parse_scrapes_all_items(self):
         """Test if the spider scrapes books and pagination links."""
         pass
