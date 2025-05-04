420ebb962f049eb985615cfc1b8beae17365b5f9
clangv <issayvega@gmail.com>
2025-05-04 21:08:44 +0800

    not yet finished

--- books/books/test_book.py
+++ books/books/test_book.py
@@ -3,12 +3,11 @@ from scrapy.http import HtmlResponse
 from books.spider.book import BookSpider
 
 class BookSpiderTest(unitest.TestCase):
-
     def setUp(self):
         self.spider = BookSpider()
         self.example_html = """
             Insert the example HTML here
-    """
+        """
         self.response =HtmlResponse(
             url="https://books.toscrape.com",
             body=self.example_html,
@@ -16,7 +15,12 @@ class BookSpiderTest(unitest.TestCase):
         )
     def test_parse_scrapes_all_items(self):
         """Test if the spider scrapes books and pagination links."""
-        pass
+        book_items = []
+        pagination_requests = []
+
+        self.assertEqual(len(book_items), 2)
+        self.assertEqual(len(pagination_requests), 1)
+        # pass
 
     def test_parse_scrapes_correct_book_information(self):
         """Test if the spider scrapes the correct information for each book."""
