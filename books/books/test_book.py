d3ae8eb5e5a0276a531a02118b148e9ba72c06a5
clangv <issayvega@gmail.com>
2025-05-07 21:12:31 +0800

    You start by adding an import for Request from scrapy.http. You’re using this class to compare whether the pagination request that the framework should generate is part of the results.Keep in mind that .parse() is a generator. For convenience, you collect all the items it yields into a list

--- books/books/test_book.py
+++ books/books/test_book.py
@@ -1,5 +1,6 @@
 import unitest
-from scrapy.http import HtmlResponse
+from scrapy.http import HtmlResponse, request
+from books.items import BooksItem
 from books.spider.book import BookSpider
 
 class BookSpiderTest(unitest.TestCase):
@@ -15,8 +16,14 @@ class BookSpiderTest(unitest.TestCase):
         )
     def test_parse_scrapes_all_items(self):
         """Test if the spider scrapes books and pagination links."""
-        book_items = []
-        pagination_requests = []
+        # Collect the items produced by the generator in a list
+        # so that it's possible to iterate over it more than once.
+        results = list(self.spider.parse(self.response))
+
+        # There should be two book items and onne pagination request
+        book_items = [item for item in results if isinstance(item, BooksItem)]
+
+        pagination_requests = [item for item in results if isinstance(item, request)]
 
         self.assertEqual(len(book_items), 2)
         self.assertEqual(len(pagination_requests), 1)
